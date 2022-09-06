f978d743b4bbbaa3d06cd7963a7215380a2e2bdb
shivamofficial <91952992+shivamofficial@users.noreply.github.com>
2022-09-06 10:42:34 +0530

    Create reverse string.cpp

---
+++ reverse string.cpp
@@ -0,0 +1,20 @@
+// using recursion
+
+void reverse(string& str,int i,int j)
+{
+    if(i>j)
+    {
+        return;
+    }
+    swap(str[i],str[j]);
+    i++;
+    j--;
+    reverse(str,i,j);
+}
+string reverseString(string str)
+{
+	// Write your code here.
+    reverse(str,0,str.length()-1);
+    return str;
+    
+}
